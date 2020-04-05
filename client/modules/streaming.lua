0f388f9e27e154a48787d27255de4044000a6459
Smallo <smallo92@gmail.com>
2020-04-05 20:13:09 +1000

    Added vector support and removed unnecessary requests
    - Added vector support for a few functions in `client/functions.lua` this will allow for vectors to be passed by also supports the old method of using tables
    - Cleaned up some of these functions to remove unnecessary items and fixed formatting
    - Removed duplicate requests in the while not loops in `client/modules/streaming,lua` also removed the unnecessary GetHashKey calls

--- client/modules/streaming.lua
+++ client/modules/streaming.lua
@@ -1,11 +1,8 @@
-function ESX.Streaming.RequestModel(modelHash, cb)
-	modelHash = (type(modelHash) == 'number' and modelHash or GetHashKey(modelHash))
-
-	if not HasModelLoaded(modelHash) and IsModelInCdimage(modelHash) then
-		RequestModel(modelHash)
-
-		while not HasModelLoaded(modelHash) do
-			Citizen.Wait(1)
+function ESX.Streaming.RequestModel(model, cb)
+	if not HasModelLoaded(model) and IsModelInCdimage(model) then
+		RequestModel(model)
+		while not HasModelLoaded(model) do
+			Citizen.Wait(0)
 		end
 	end
 
@@ -17,9 +14,8 @@ end
 function ESX.Streaming.RequestStreamedTextureDict(textureDict, cb)
 	if not HasStreamedTextureDictLoaded(textureDict) then
 		RequestStreamedTextureDict(textureDict)
-
 		while not HasStreamedTextureDictLoaded(textureDict) do
-			Citizen.Wait(1)
+			Citizen.Wait(0)
 		end
 	end
 
@@ -31,9 +27,8 @@ end
 function ESX.Streaming.RequestNamedPtfxAsset(assetName, cb)
 	if not HasNamedPtfxAssetLoaded(assetName) then
 		RequestNamedPtfxAsset(assetName)
-
 		while not HasNamedPtfxAssetLoaded(assetName) do
-			Citizen.Wait(1)
+			Citizen.Wait(0)
 		end
 	end
 
@@ -45,9 +40,8 @@ end
 function ESX.Streaming.RequestAnimSet(animSet, cb)
 	if not HasAnimSetLoaded(animSet) then
 		RequestAnimSet(animSet)
-
 		while not HasAnimSetLoaded(animSet) do
-			Citizen.Wait(1)
+			Citizen.Wait(0)
 		end
 	end
 
@@ -59,9 +53,8 @@ end
 function ESX.Streaming.RequestAnimDict(animDict, cb)
 	if not HasAnimDictLoaded(animDict) then
 		RequestAnimDict(animDict)
-
 		while not HasAnimDictLoaded(animDict) do
-			Citizen.Wait(1)
+			Citizen.Wait(0)
 		end
 	end
 
@@ -73,9 +66,8 @@ end
 function ESX.Streaming.RequestWeaponAsset(weaponHash, cb)
 	if not HasWeaponAssetLoaded(weaponHash) then
 		RequestWeaponAsset(weaponHash)
-
 		while not HasWeaponAssetLoaded(weaponHash) do
-			Citizen.Wait(1)
+			Citizen.Wait(0)
 		end
 	end
 
