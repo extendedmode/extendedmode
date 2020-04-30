f3d8c571f2141e0fdb81878b0c67462afda091d4
Kanersps <kanepetra@gmail.com>
2020-04-30 18:15:39 +0200

    tweak: __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,7 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
 
-description 'ES Extended'
+description 'ExtendedMode'
 
 version '1.1.0'
 
