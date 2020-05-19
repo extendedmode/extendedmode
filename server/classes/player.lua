669eb342dd2a20b162e5a5ed0127c7bdf76dd009
JnKTechstuff <7122547+JnKTechstuff@users.noreply.github.com>
2020-05-18 21:21:53 -0400

    Update player.lua

--- server/classes/player.lua
+++ server/classes/player.lua
@@ -315,7 +315,15 @@ function CreateExtendedPlayer(playerId, identifier, group, accounts, inventory, 
 	self.canCarryItem = function(name, count)
 		local currentWeight, itemWeight = self.weight, ESX.Items[name].weight
 		local newWeight = currentWeight + (itemWeight * count)
-
+		local inventoryitem = self.getInventoryItem(name)
+		
+		if ESX.Items[name].limit ~= nil or ESX.Items[name].limit ~= -1 then
+			if count > ESX.Items[name].limit then
+				return false
+			elseif (inventoryitem.count + count) > ESX.Items[name].limit then
+				return false
+			end
+		end
 		return newWeight <= self.maxWeight
 	end
 
