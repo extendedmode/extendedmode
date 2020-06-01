c864ed49b6d29b31dc238e03b48a7134129d334f
Alexander Schilling <alexander.schilling.miranda@gmail.com>
2020-06-01 17:19:29 -0400

    fix (client) - Coords updating twice
    Coords are already updating if distance is greater than 10, it was also checking if it was greater than 1, and updating again.

--- client/main.lua
+++ client/main.lua
@@ -550,9 +550,6 @@ CreateThread(function()
 			playerHeading = ESX.Math.Round(GetEntityHeading(playerPed), 1)
 			formattedCoords = {x = ESX.Math.Round(playerCoords.x, 1), y = ESX.Math.Round(playerCoords.y, 1), z = ESX.Math.Round(playerCoords.z, 1), heading = playerHeading}
 			TriggerServerEvent('esx:updateCoords', formattedCoords)
-			if distance > 1 then
-				TriggerServerEvent('esx:updateCoords', formattedCoords)
-			end
 		end
 	end
 end)
