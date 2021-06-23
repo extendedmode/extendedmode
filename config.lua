53e22f5cc76c9ec720355f2e0ca2753e353e5b46
carlossdev-svg <85409243+carlossdev-svg@users.noreply.github.com>
2021-06-23 23:22:25 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ Config.StartingAccountMoney = {bank = 50000}
 
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.DisableWantedLevel   = true
-Config.EnableHud            = false -- enable the default hud? Display current job and accounts (black, bank & cash)
+Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.EnablePvP            = true -- enable pvp?
 Config.MaxWeight            = 24000   -- the max inventory weight without backpack(this is in grams, not kg!)
 
